d9d834b9b2f20c7292f38a159b51629c1bb0cdf5
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2021-02-16 11:40:08 +0300

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
 # coding: utf-8
-
-# In[4]:
 
 
 # Импорты
@@ -17,9 +14,6 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-
-
-# In[5]:
 
 
 def newline(p1, p2, color=None):
@@ -41,11 +35,7 @@ def newline(p1, p2, color=None):
     return l
 
 
-# In[7]:
-
-
 # Soft-margin SVM https://habr.com/ru/company/ods/blog/484148/
-
 def add_bias_feature(a):
     """Код функции расширения вектора признаков
     """
@@ -53,6 +43,7 @@ def add_bias_feature(a):
     a_extended[:,:-1] = a
     a_extended[:,-1] = int(1)  
     return a_extended
+
 
 class CustomSVM(object):
 
@@ -117,7 +108,8 @@ class CustomSVM(object):
                     self._w = self._w - self._etha*self._alpha*self._w/self._epochs
                     tr_loss += self.soft_margin_loss(X_train[i],Y_train[i])
                 else: # классифицируем неверно или попадаем на полосу разделения при 0<m<1
-                    self._w = self._w +                    self._etha*(Y_train[i]*X_train[i] - self._alpha*self._w/self._epochs)
+                    self._w = self._w + \
+                    self._etha*(Y_train[i]*X_train[i] - self._alpha*self._w/self._epochs)
                     tr_err += 1
                     tr_loss += self.soft_margin_loss(X_train[i],Y_train[i])
                 self.history_w.append(self._w)
@@ -125,7 +117,7 @@ class CustomSVM(object):
                 val_loss += self.soft_margin_loss(X_val[i], Y_val[i])
                 val_err += (Y_val[i]*np.dot(self._w,X_val[i])<1).astype(int)
             if verbose:
-                print('epoch {}. Errors={}. Mean Hinge_loss={}'                      .format(epoch,err,loss))
+                print(f'epoch {epoch}. Errors={err}. Mean Hinge_loss={loss}')
             train_errors.append(tr_err)
             val_errors.append(val_err)
             train_loss_epoch.append(tr_loss)
@@ -150,9 +142,6 @@ class CustomSVM(object):
         return self.hinge_loss(x,y)+self._alpha*np.dot(self._w, self._w)
 
 
-# In[9]:
-
-
 # Блок подготовки данных
 iris = load_iris()
 X = iris.data
@@ -165,9 +154,6 @@ Y = (Y > 0).astype(int)*2-1
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, 
                                                     random_state=2020)
-
-
-# In[10]:
 
 
 # Блок инициализиции и обучения
@@ -184,9 +170,6 @@ plt.legend(prop={'size': 15})
 plt.show()
 
 
-# In[11]:
-
-
 d = {-1:'green', 1:'red'}
 plt.scatter(X_train[:,0], X_train[:,1], c=[d[y] for y in Y_train])
 
@@ -199,15 +182,13 @@ newline([0,-1/svm._w[1]-svm._w[2]/svm._w[1]],[-1/svm._w[0]-svm._w[2]/svm._w[0],0
 plt.show()
 
 
-# In[12]:
-
-
 # Предсказываем после обучения
 y_pred = svm.predict(X_test)
 y_pred[y_pred != Y_test] = -100 
 print('Количество ошибок для отложенной выборки: ', 
       (y_pred == -100).astype(int).sum())
 
+# Блок визуализации получившейся разделительной полосы
 d1 = {-1:'lime', 1:'m', -100: 'black'} # black = classification error
 plt.scatter(X_test[:,0], X_test[:,1], c=[d1[y] for y in y_pred])
 newline([0,-svm._w[2]/svm._w[1]],[-svm._w[2]/svm._w[0],0], 'blue')
@@ -216,10 +197,3 @@ newline([0,1/svm._w[1]-svm._w[2]/svm._w[1]],[1/svm._w[0]-svm._w[2]/svm._w[0],0])
 #w0*x_i[0]+w1*x_i[1]+w2*1=-1
 newline([0,-1/svm._w[1]-svm._w[2]/svm._w[1]],[-1/svm._w[0]-svm._w[2]/svm._w[0],0]) 
 plt.show()
-
-
-# In[ ]:
-
-
-
-
